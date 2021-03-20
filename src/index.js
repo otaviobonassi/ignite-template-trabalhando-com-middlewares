c9b56488cda3c7cbe92497cef177c31078525166
otaviobonassi <obonassi@gmail.com>
2021-03-20 15:11:15 -0300

    done

--- src/index.js
+++ src/index.js
@@ -10,19 +10,50 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const user = users.find( user => user.username === username);
+  if(!user) return response.status(404).json({error: "User not found"});
+
+  request.user = user;
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+  const moreThan10ToDos = user.todos.length >= 10;
+
+  if(!user.pro && moreThan10ToDos) return response.status(403).json({error: "Not valid plan"});
+
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  if(!user) return response.status(404).json({error: "User not found"})
+
+  const valid = validate(id);
+  if(!valid) return response.status(400).json({error: "Invalid UUID"});
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if(!todo) return response.status(404).json({error: "ToDo not found"});
+
+  request.user = user;
+  request.todo = todo;
+  next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+  if(!user) return response.status(404).json({error: "User not found"});
+
+  request.user = user;
+  next()
 }
 
 app.post('/users', (request, response) => {
@@ -117,7 +148,7 @@ app.delete('/todos/:id', checksExistsUserAccount, checksTodoExists, (request, re
 
   user.todos.splice(todoIndex, 1);
 
-  return response.status(204).send();
+  return response.send(204);
 });
 
 module.exports = {
